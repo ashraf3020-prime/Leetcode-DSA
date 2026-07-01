4dbf9f08fea0b05cded08fadc5bdd21ed9657022
ashraf ansari <miyaashraf3020@gmail.com>
2026-07-01 17:19:50 +0530

    Time: 23 ms (79.20%) | Memory: 90.6 MB (37.23%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,40 @@
+// here time limit exceed again 
+
+
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         for(int i =0;i<nums.length;i++){
+//             for(int j =i+1;j<nums.length;j++){
+//                 if(nums[i]==nums[j]&&Math.abs(i-j)<=k){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(nums[i])) {
+
+                int previousIndex = map.get(nums[i]);
+
+                if (i - previousIndex <= k) {
+                    return true;
+                }
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
