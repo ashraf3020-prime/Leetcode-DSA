e72f8f856edd1f48cd9f344e1df88ec6a9aef83f
ashraf ansari <miyaashraf3020@gmail.com>
2026-07-06 14:46:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (76.88%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,14 +1,47 @@
+//this is my code accepted in leetcode 
+
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         for(int i = 0; i<nums.length;i++){
+//             for(int j = i+1;j<nums.length;j++){
+//                 if(nums[i]>nums[j]){
+//                     int temp = nums[i];
+//                     nums[i] = nums[j];
+//                     nums[j] = temp;
+//                 }
+//             }
+//         }
+        
+//     }
+// }
+
+// now this one is chatgpt's code
 class Solution {
     public void sortColors(int[] nums) {
-        for(int i = 0; i<nums.length;i++){
-            for(int j = i+1;j<nums.length;j++){
-                if(nums[i]>nums[j]){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-            }
+
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+
+        for (int num : nums) {
+            if (num == 0)
+                zero++;
+            else if (num == 1)
+                one++;
+            else
+                two++;
         }
-        
+
+        int index = 0;
+
+        while (zero-- > 0)
+            nums[index++] = 0;
+
+        while (one-- > 0)
+            nums[index++] = 1;
+
+        while (two-- > 0)
+            nums[index++] = 2;
     }
 }
